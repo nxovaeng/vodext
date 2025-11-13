ff2b6d39b72c6a0ed9bf6c03ff7ad315c3c41362
lvsekafen <lvsekafen@outlook.com>
2025-11-13 22:50:02 +0800

    add pipishi.cc test code

--- MacCMS/src/main/kotlin/zronest/MacCMSPlugin.kt
+++ MacCMS/src/main/kotlin/zronest/MacCMSPlugin.kt
@@ -7,6 +7,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 class MacCMSPlugin: BasePlugin() {
     override fun load() {
         // 注册所有 MacCMS 风格的站点
+        registerMainAPI(BaseMacCmsProvider())
         registerMainAPI(ReboVodProvider())
         registerMainAPI(ZjkrmvProvider())
     }
